5c68cf6ec74e2073c7c7ddf573916e4d3c499410
zachung <zach50931@gmail.com>
2025-01-09 14:16:01 +0800

    refactor: [3B]整理操作策略

--- 3B/internal/ai.go
+++ 3B/internal/ai.go
@@ -8,41 +8,37 @@ import (
 )
 
 type AI struct {
+	role *Role
 	seed int
 }
 
-func (a *AI) Action(role *Role, targetTroop *Troop) {
-	defer func() {
-		a.seed += 1
-	}()
-	s := a.selectSkill(role)
-	targetCount := s.Area[skill.Enemy]
-	if targetCount != 0 {
-		targets := targetTroop.AliveRoles()
-		if targetCount > 0 && targetCount < len(targets) {
-			a.selectTarget(role, len(targets))
-			// 選擇敵方目標
-			targets = selectTarget(role, targets)
-		}
-		castSkill(role, s, targets)
-	}
-}
-
-func (a *AI) selectSkill(role *Role) *skill.Skill {
+func (a *AI) Action(targetTroop *Troop) {
 	var s *skill.Skill
-	actionCount := len(role.Skills)
 	for {
-		role.controller.AddCommand(strconv.Itoa(a.seed % actionCount))
-		s = selectSkill(role)
+		s = a.selectSkill()
 		if s != nil {
 			break
 		}
 		a.seed += 1
 	}
-	return s
+	targetCount := s.Area[skill.Enemy]
+	if targetCount != 0 {
+		targets := targetTroop.AliveRoles()
+		if targetCount > 0 && targetCount < len(targets) {
+			targets = a.selectTarget(targets, targetCount)
+		}
+		a.role.CastSkill(s, targets)
+	}
+	a.seed += 1
 }
 
-func (a *AI) selectTarget(role *Role, n int) {
+func (a *AI) selectSkill() *skill.Skill {
+	a.role.controller.AddCommand(strconv.Itoa(a.seed % len(a.role.Skills)))
+	return a.role.SelectSkill()
+}
+
+func (a *AI) selectTarget(enemies []*Role, targetCount int) []*Role {
+	n := len(enemies)
 	var ints []int
 	for i := a.seed % n; i < n; i++ {
 		if i == n {
@@ -51,5 +47,7 @@ func (a *AI) selectTarget(role *Role, n int) {
 		ints = append(ints, i)
 	}
 	cmd := strings.Trim(strings.Join(strings.Split(fmt.Sprint(ints), " "), ", "), "[]")
-	role.controller.AddCommand(cmd)
+	a.role.controller.AddCommand(cmd)
+
+	return a.role.SelectTarget(enemies, targetCount)
 }
--- 3B/internal/role.go
+++ 3B/internal/role.go
@@ -1,6 +1,10 @@
 package internal
 
-import "fmt"
+import (
+	"3B/internal/skill"
+	"fmt"
+	"strings"
+)
 
 type Role struct {
 	Name        string
@@ -8,19 +12,26 @@ type Role struct {
 	Skills      []string
 	State       string
 	troop       *Troop
-	ai          AI
+	actor       Actor
 	commands    []string
 	controller  *Controller
 }
 
 func NewRole(name string, hp, mp, str int, skills []string) *Role {
-	ai := AI{}
-	return &Role{
+	role := &Role{
 		Name: name, Hp: hp, Mp: mp, Str: str, Skills: skills,
 		State:      "正常",
-		ai:         ai,
 		controller: NewController(),
 	}
+	var actor Actor
+	if name == "英雄" {
+		actor = &Round{role: role}
+	} else {
+		actor = &AI{role: role}
+	}
+	role.actor = actor
+
+	return role
 }
 
 func (r *Role) IsDead() bool {
@@ -33,9 +44,76 @@ func (r *Role) GetName() string {
 
 func (r *Role) Action(targetTroop *Troop) {
 	fmt.Printf("輪到 %s (HP: %d, MP: %d, STR: %d, State: %s)。\n", r.GetName(), r.Hp, r.Mp, r.Str, r.State)
-	if r.Name == "英雄" {
-		RunRound(r, targetTroop)
+	r.actor.Action(targetTroop)
+}
+
+func (r *Role) SelectSkill() *skill.Skill {
+	var actionList []string
+	for i, s := range r.Skills {
+		actionList = append(actionList, fmt.Sprintf("(%d) %s", i, s))
+	}
+	// print 選擇行動
+	fmt.Printf("選擇行動：%v\n", strings.Join(actionList, " "))
+
+	i := r.controller.PullCommand()[0]
+
+	var s *skill.Skill
+	str := r.Skills[i]
+	switch str {
+	case "普通攻擊":
+		return &skill.BasicAttack
+	case "水球":
+		s = &skill.Waterball
+	case "火球":
+		s = &skill.Fireball
+	}
+	if s == nil {
+		return nil
+	}
+	if r.Mp < s.MpCost {
+		fmt.Println("你缺乏 MP，不能進行此行動。")
+		return nil
+	}
+	return s
+}
+
+func (r *Role) SelectTarget(enemies []*Role, targetCount int) (targets []*Role) {
+	// 需要選擇敵方目標
+	var targetList []string
+	for i, t := range enemies {
+		targetList = append(targetList, fmt.Sprintf("(%d) %s", i, t.GetName()))
+	}
+	fmt.Printf("選擇 %d 位目標: %s\n", targetCount, strings.Join(targetList, " "))
+
+	// 選擇敵方目標
+	command := r.controller.PullCommand()
+	for _, i := range command {
+		targets = append(targets, enemies[i])
+	}
+
+	return
+}
+
+func (r *Role) CastSkill(s *skill.Skill, targets []*Role) {
+	r.Mp -= s.MpCost
+	var str []string
+	for _, enemy := range targets {
+		str = append(str, enemy.GetName())
+	}
+	if s == &skill.BasicAttack {
+		fmt.Printf("%s 攻擊 %s。\n", r.GetName(), strings.Join(str, ", "))
 	} else {
-		r.ai.Action(r, targetTroop)
+		fmt.Printf("%s 對 %s 使用了 %s。\n", r.GetName(), strings.Join(str, ", "), s.Name)
+	}
+	for _, enemy := range targets {
+		damage := s.Damage
+		if s == &skill.BasicAttack {
+			damage = r.Str
+		}
+		enemy.Hp -= damage
+		fmt.Printf("%s 對 %s 造成 %d 點傷害。\n", r.GetName(), enemy.GetName(), damage)
+		if enemy.IsDead() {
+			fmt.Printf("%s 死亡。\n", enemy.GetName())
+		}
 	}
 }
